e9834996e320c8993809dce6ac7b4ed6961d2d8e
Sergey Sterekhov <sergeyst4@gmail.com>
2024-06-10 16:33:06 +0300

    Print Hello In English
    HomeWork1: Print Hello In English

---
+++ Print Hello In Russian/Print Hello In Russian.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	cout << "Hello World!!" << endl;
+}
+
+
+//void main()
+//{
+//	setlocale(LC_ALL, "Rus");
+//	cout << "Привет!!" << endl;
+//}
